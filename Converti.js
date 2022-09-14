7e5a68838512704510c68a36e8f57d337f8cdf3e
Nando <nandosorbello@live.it>
2022-09-14 20:30:14 +0200

    End

--- Converti.js
+++ Converti.js
@@ -1,1 +1,29 @@
 import fs from "fs-extra";
+import path from "path";
+
+const ChapterDir = "./Parse";
+const allChapterList = await fs.readdir(ChapterDir);
+
+let counter = 0;
+
+for (const chapterFolder of allChapterList) {
+  const pageList = await fs.readdir(path.join(ChapterDir, chapterFolder));
+  console.log("Processing Chapter => " + path.join(ChapterDir, chapterFolder));
+
+  for (const page of pageList) {
+    console.log("Now Moving Page => " + path.join(chapterFolder, page));
+    const fileBuffer = await fs.readFile(
+      path.join(ChapterDir, chapterFolder, page)
+    );
+
+    await fs.writeFile(
+      path.join(
+        ChapterDir,
+        "page_" + counter.toString().padStart(3, "0") + ".jpg"
+      ),
+      fileBuffer
+    );
+
+    counter++;
+  }
+}
