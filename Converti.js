28f6e259d8444b5bd61a3ff9c8900dca1219ae74
Nando <nandosorbello@live.it>
2022-10-05 22:25:13 +0200

    Add Output Directory

--- Converti.js
+++ Converti.js
@@ -2,15 +2,23 @@ import fs from "fs-extra";
 import path from "path";
 
 const ChapterDir = "./Parse";
+const OutputDir = "./Output";
 const allChapterList = await fs.readdir(ChapterDir);
 
 let counter = 0;
+
+if (await fs.pathExists(OutputDir)) {
+  await fs.rm(OutputDir, { recursive: true, force: true });
+}
+
+await fs.mkdir(OutputDir);
 
 for (const chapterFolder of allChapterList) {
   const pageList = await fs.readdir(path.join(ChapterDir, chapterFolder));
   console.log("Processing Chapter => " + path.join(ChapterDir, chapterFolder));
 
   for (const page of pageList) {
+    if (page === ".nomedia") continue;
     console.log("Now Moving Page => " + path.join(chapterFolder, page));
     const fileBuffer = await fs.readFile(
       path.join(ChapterDir, chapterFolder, page)
@@ -18,7 +26,7 @@ for (const chapterFolder of allChapterList) {
 
     await fs.writeFile(
       path.join(
-        ChapterDir,
+        OutputDir,
         "page_" + counter.toString().padStart(3, "0") + ".jpg"
       ),
       fileBuffer
